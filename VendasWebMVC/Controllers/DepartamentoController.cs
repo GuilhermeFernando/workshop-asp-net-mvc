a69186c6eece90c617f0e146effa4c4d55c01109
Guilherme Fernando <guifermach@hgtmail.com>
2021-07-20 19:31:54 -0300

    Primeiro commit

---
+++ VendasWebMVC/Controllers/DepartamentoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Eletronico" });
+            list.Add(new Departamento { Id = 2, Nome = "Moda" });
+
+            return View(list);
+            return View();
+        }
+    }
+}
